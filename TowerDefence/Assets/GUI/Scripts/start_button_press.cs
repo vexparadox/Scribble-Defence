374218ea159e626500e51641f0e5cef4a633edfa
William Meaton <will.meaton@gmail.com>
2015-01-27 12:25:29 +0000

    Added a basic main menu for test
    -Open the MainMenu scene and run to see the main menu, it will load
    Level1.scene on click
    
    -Fixed some gitignore issues

---
+++ TowerDefence/Assets/GUI/Scripts/start_button_press.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start_button_press : MonoBehaviour {
+
+	public Sprite button;
+	public Sprite button_down;
+
+	void OnMouseEnter(){
+		gameObject.GetComponent<SpriteRenderer> ().sprite = button_down;
+	}
+	void OnMouseExit(){
+		gameObject.GetComponent<SpriteRenderer> ().sprite = button;
+	}
+	void OnMouseDown(){
+		Application.LoadLevel ("Level1");
+	}
+}
