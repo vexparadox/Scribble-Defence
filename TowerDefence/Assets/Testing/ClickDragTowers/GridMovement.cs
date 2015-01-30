0c3236b4a1d296feb96dc0e016f5dd2d71ed68e3
William Meaton <will.meaton@gmail.com>
2015-01-30 21:54:05 +0000

    Worked through and arranged what we did today
    Added my click-drag testing of objects from a menu - click and drag the red box and another will appear where dropped and the original shall snap back to it's origin point

---
+++ TowerDefence/Assets/Testing/ClickDragTowers/GridMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridMovement : MonoBehaviour {
+	public float speed = 10f;
+	private Vector2 target;
+	// Use this for initialization
+	void Start () {
+		target = transform.position;
+		
+	}
+	// Update is called once per frame
+	void Update () {
+		target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		target.x = transform.position.x;
+		target.y = transform.position.y;
+		transform.position = new Vector2(target.x, target.y);
+	}
+}
