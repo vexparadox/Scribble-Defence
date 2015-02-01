620bd515aa2ee4cdcd0aa4d2c1d7e528335ac1e4
Michael King <michael.j.king@live.co.uk>
2015-02-01 14:53:20 +0000

    Testing of tower shooting so far.

---
+++ TowerDefence/Assets/MikesTestingThings/TestTowerScript.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var TowerAmmo:GameObject;
+
+function Start () {
+Shoot();
+}
+
+function Update () {
+
+}
+
+function Shoot(){
+ Debug.Log("Shot");
+ Instantiate(TowerAmmo, Vector3(4,1,0), Quaternion.identity);
+ }
---
+++ TowerDefence/Assets/MikesTestingThings/TesterAmmoScript.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+var CurrentShortestObject : Transform;
+
+
+function Start () {
+ transform.LookAt(FindNearest());
+}
+
+function Update () {
+
+}
+
+function FindNearest(){
+
+ var EnemyFinder = GameObject.FindGameObjectsWithTag("Enemy"); 
+ var CurrentShortestDist = 999999999999;
+ 
+ for (var i = 0 ; i<EnemyFinder.length ; i++){
+  var TryingEnemy = EnemyFinder[i].transform.position;
+ var TryingDistance = 999999999999999999;
+ TryingDistance= Vector3.Distance(TryingEnemy, transform.localPosition);
+ 	if(TryingDistance<CurrentShortestDist){
+  CurrentShortestObject = TryingEnemy;
+     CurrentShortestDist=TryingDistance;
+  }
+   }
+   return CurrentShortestObject;
+} 
