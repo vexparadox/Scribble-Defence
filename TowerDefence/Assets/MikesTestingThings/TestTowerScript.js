8877449902db48ae08a498d3099de86454742119
Michael King <michael.j.king@live.co.uk>
2015-02-01 18:25:32 +0000

    Finished tower orienting to the closest enemy and then shooting when the enemy is within range.

--- TowerDefence/Assets/MikesTestingThings/TestTowerScript.js
+++ TowerDefence/Assets/MikesTestingThings/TestTowerScript.js
@@ -1,16 +1,43 @@
 ﻿#pragma strict
-
+var CurrentShortestObject: Transform;
 public var TowerAmmo:GameObject;
+public var Range = 90;
+public var Reload = 5;
+var hasShot = false;
 
-function Start () {
-//Shoot();
-}
+
 
 function Update () {
-
+Fire();	
 }
 
 function Shoot(){
  Debug.Log("Shot");
- Instantiate(TowerAmmo, Vector3(4,1,0), Quaternion.identity);
+ Instantiate(TowerAmmo, transform.localPosition, Quaternion.identity);
  }
+ 
+ function Fire(){
+ if(!hasShot){
+ var EnemyFinder = GameObject.FindGameObjectsWithTag("Enemy"); 
+ var CurrentShortestDist = 9999;
+ 
+	for (var i = 0 ; i<EnemyFinder.length ; i++){
+  	var TryingEnemy = EnemyFinder[i].transform.position;
+ 	var TryingDistance = 99999;
+ 	TryingDistance= Vector2.Distance(TryingEnemy, transform.localPosition);
+ 		if(TryingDistance<CurrentShortestDist){
+  			 CurrentShortestObject = EnemyFinder[i].transform;
+    		 CurrentShortestDist=TryingDistance;
+    	}
+    }
+     var dir = CurrentShortestObject.transform.position - transform.position; // See Below
+ var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90; // See Below
+ transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); // Thanks to http://answers.unity3d.com/questions/654222/make-sprite-look-at-vector2-in-unity-2d-1.html for the lookat alternative
+	if(Range>Vector2.Distance(CurrentShortestObject.transform.position, transform.localPosition)){
+	hasShot=true;
+	Shoot();
+	yield WaitForSeconds(Reload);
+	hasShot=false;
+	}
+	}
+} 
--- TowerDefence/Assets/MikesTestingThings/TesterAmmoScript.js
+++ TowerDefence/Assets/MikesTestingThings/TesterAmmoScript.js
@@ -1,14 +1,18 @@
 ﻿#pragma strict
 var CurrentShortestObject: Transform;
-var debugger: Transform;
+public var bulletspeed = 10f;
 
 function Start () {
-
 FindNearest();
- transform.LookAt(CurrentShortestObject.transform.position);
- Debug.Log(debugger.position);
- Debug.Log(debugger.rotation);
-}
+//transform.LookAt(CurrentShortestObject.transform.position); //- Apparently gives 4 rotation values.
+
+ var dir = CurrentShortestObject.transform.position - transform.position; // See Below
+ var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90; // See Below
+ transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); // Thanks to http://answers.unity3d.com/questions/654222/make-sprite-look-at-vector2-in-unity-2d-1.html for the lookat alternative
+ var xforce = Mathf.Cos(rigidbody2D.rotation*Mathf.Deg2Rad+(Mathf.PI/2))*bulletspeed;
+ var yforce = Mathf.Sin(rigidbody2D.rotation*Mathf.Deg2Rad+(Mathf.PI/2))*bulletspeed; // Use of SOHCAH to figure out how to apply the force correctly
+ rigidbody2D.AddForceAtPosition(new Vector2(xforce,yforce), transform.position);
+ }
 
 function Update () {
 
@@ -20,14 +24,13 @@ function FindNearest(){
  var EnemyFinder = GameObject.FindGameObjectsWithTag("Enemy"); 
  var CurrentShortestDist = 9999;
  
- for (var i = 0 ; i<EnemyFinder.length ; i++){
-  var TryingEnemy = EnemyFinder[i].transform.position;
- var TryingDistance = 99999;
- TryingDistance= Vector3.Distance(TryingEnemy, transform.localPosition);
- 	if(TryingDistance<CurrentShortestDist){
-  CurrentShortestObject = EnemyFinder[i].transform;
-     CurrentShortestDist=TryingDistance;
-     Debug.Log(CurrentShortestObject);
-  }
-   }
+	for (var i = 0 ; i<EnemyFinder.length ; i++){
+  	var TryingEnemy = EnemyFinder[i].transform.position;
+ 	var TryingDistance = 99999;
+ 	TryingDistance= Vector2.Distance(TryingEnemy, transform.localPosition);
+ 		if(TryingDistance<CurrentShortestDist){
+  			 CurrentShortestObject = EnemyFinder[i].transform;
+    		 CurrentShortestDist=TryingDistance;
+    	}
+    }
 } 
