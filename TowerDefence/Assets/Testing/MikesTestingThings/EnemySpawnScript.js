e99c7d63fef9e4466e8656a1b4f3df3b9f9213f3
William Meaton <will.meaton@gmail.com>
2015-02-10 11:26:10 +0000

    Had a go at fixing the spawn timer, seems to be working now. Press the space bar ONCE to spawn the enemies. We can use _GM.SpawnGrunt(); to spawn enemies later etc.

--- TowerDefence/Assets/Testing/MikesTestingThings/EnemySpawnScript.js
+++ TowerDefence/Assets/Testing/MikesTestingThings/EnemySpawnScript.js
@@ -2,24 +2,35 @@
 var gruntWaves: int[];
 public var numberOfWaves = 1;
 public var gruntEnemy:GameObject;
-var currentWaveAmount = 0;
-var currentWave = 0;
+private var currentWaveAmount = 0;
+private var currentWave = 0;
 
 function Start () {
-gruntWaves = new int[numberOfWaves];
-gruntWaves[0] = 5;
-
+	//grunt waves holds the amount of grunts in each wave
+	gruntWaves = new int[numberOfWaves];
+	for (var i = 0; i < numberOfWaves; i++){
+	//formula can be changed
+		gruntWaves[i] = (i*2)+3;
+	}
+	Debug.Log(gruntWaves[0]);
 }
 
 function Update () {
-SpawnGrunt();
+	if (Input.GetKeyDown ("space")){
+		SpawnGrunt();
+	}
 }
 
 function SpawnGrunt(){
-if(currentWaveAmount<gruntWaves[currentWave]){
-Debug.Log(currentWaveAmount);
-Instantiate(gruntEnemy, Vector2(currentWaveAmount,currentWaveAmount), Quaternion.identity);
-currentWaveAmount++;
-yield WaitForSeconds(10);
-}
+	//Using a while loop seems to fix the non spawning issue, it was a logic problem.... I think
+	//while there are less enemies than expected keep spawning them
+	while(currentWaveAmount<gruntWaves[currentWave]){
+		Debug.Log(currentWaveAmount);
+		//create the enemy
+		Instantiate(gruntEnemy, Vector2(currentWaveAmount,currentWaveAmount), Quaternion.identity);
+		//increase the number of enemyes on screen
+		currentWaveAmount++;
+		//wait for respawn time
+		yield WaitForSeconds(2);
+	}
 }
