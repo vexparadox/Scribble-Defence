00c2ff2ba264ed5a3c20a29ea93124f54e46f76f
William Meaton <will.meaton@gmail.com>
2015-02-13 16:43:28 +0000

    Attempted to do wave spawning

--- TowerDefence/Assets/Prefabs/Enemies/Scripts/EnemySpawnScript.js
+++ TowerDefence/Assets/Prefabs/Enemies/Scripts/EnemySpawnScript.js
@@ -1,39 +1,96 @@
 ﻿#pragma strict
 //We can either have a different script for each enemy type, or have multiple functions in one script both of these are fine
-private var gruntWaves: int[]; // deals with the 
+/*
+These 2D arrays hold [waveNumber][perWave];
+*/
+private var gruntsPerWave: int[];
+private var tanksPerWave: int[];
+private var speedyPerWave: int[];
+
 public var enemyParentObject:GameObject; // this is what all enemies will be a child of
 public var SpawnPoint:Transform; // this is the spawn point of all the enemies
-public var gruntEnemyPrefab:GameObject; // this is the objet that is the enemy
 
-public var timeBetweenEnemies:int; //hold the respawn time between enemies
-public var numberOfWaves = 1; // this is the number of waves that will occur
-private var currentWaveAmount = 0; // the current amount of enemies in a wave - this will have to be seperate for 
+public var gruntEnemyPrefab:GameObject; // this is the objet that is the enemy
+public var tankEnemyPrefab:GameObject;
+public var bossEnemyPrefab:GameObject;
+public var speedEnemyPrefab:GameObject;
+
+public var timeBetweenEnemies: int; //hold the respawn time between enemies
+public var numberOfWaves: int; // this is the number of waves that will occur
+
+private var currentGruntCount = 0; // the current amount of enemies in a wave - this will have to be seperate for 
+private var currentTankCount = 0;
+private var currentSpeedyCount = 0;
+
+private var roundOver: boolean = false;
 private var currentWave = 0; //the current wave we're on
 
 function Start () {
-	//grunt waves holds the amount of grunts in each wave
-	gruntWaves = new int[numberOfWaves];
-	for (var i = 0; i < numberOfWaves; i++){
-	//formula can be changed
-		gruntWaves[i] = (i*2)+10;
+	gruntsPerWave = new int[numberOfWaves];
+	tanksPerWave = new int[numberOfWaves];
+	speedyPerWave = new int[numberOfWaves];
+	
+	for(var i = 0; i < numberOfWaves; i++){
+		if(i == 0){
+			gruntsPerWave[i] = 5;
+		} else{
+			gruntsPerWave[i] = i*5;
+		}
+		tanksPerWave[i] = 5;
+		speedyPerWave[i] = i;
 	}
-	//spawn the grunts on launch
-	SpawnGrunt();
+	Spawn();
 }
 
-function SpawnGrunt(){
-	//holds the new enemy so we can make
-	var newEnemy:GameObject;
-	//Using a while loop seems to fix the non spawning issue, it was a logic problem.... I think
-	//while there are less enemies than expected keep spawning them
-	while(currentWaveAmount<gruntWaves[currentWave]){
-		Debug.Log(currentWaveAmount);
-		//create the enemy at the spawn point
+function Spawn(){
+	while(!roundOver){
+	var rndChance = parseInt(Random.Range(0,2));
+	
+	switch(rndChance){
+	case 0:
+		if(currentGruntCount<gruntsPerWave[currentWave]){
+			var newGruntEnemy:GameObject;
+			newGruntEnemy = Instantiate(gruntEnemyPrefab, Vector2(SpawnPoint.position.x, SpawnPoint.position.y), Quaternion.identity);
+			newGruntEnemy.transform.parent = enemyParentObject.transform;
+			currentGruntCount++;
+		}
+	break;
+	case 1:
+		if(currentTankCount<tanksPerWave[currentWave]){
+			var newTankEnemy:GameObject;
+			newTankEnemy = Instantiate(tankEnemyPrefab, Vector2(SpawnPoint.position.x, SpawnPoint.position.y), Quaternion.identity);
+			newTankEnemy.transform.parent = enemyParentObject.transform;
+			currentTankCount++;
+		}
+	break;
+	 
+	}
+	yield WaitForSeconds(timeBetweenEnemies);
+}
+	/*
+	
+	while(currentGruntCount<gruntsPerWave[currentWave]){
+		var newGruntEnemy:GameObject;
+		newGruntEnemy = Instantiate(gruntEnemyPrefab, Vector2(SpawnPoint.position.x, SpawnPoint.position.y), Quaternion.identity);
+		newGruntEnemy.transform.parent = enemyParentObject.transform;
+		currentGruntCount++;
+ 		yield WaitForSeconds(timeBetweenEnemies);
+	}
+	//spawn tanks
+	while(currentTankCount<tanksPerWave[currentWave]){
+		var newTankEnemy:GameObject;
+		newTankEnemy = Instantiate(tankEnemyPrefab, Vector2(SpawnPoint.position.x, SpawnPoint.position.y), Quaternion.identity);
+		newTankEnemy.transform.parent = enemyParentObject.transform;
+		currentTankCount++;
+		yield WaitForSeconds(timeBetweenEnemies);
+}
+	/spawn speedys
+	while(currentSpeedyAmount<speedyPerWave[currentWave]){
+		var newEnemy:GameObject;
 		newEnemy = Instantiate(gruntEnemyPrefab, Vector2(SpawnPoint.position.x, SpawnPoint.position.y), Quaternion.identity);
 		newEnemy.transform.parent = enemyParentObject.transform;
-		//increase the number of enemyes on screen
-		currentWaveAmount++;
-		//wait for respawn time
-		yield WaitForSeconds(timeBetweenEnemies);
-	}
+		currentGruntAmount++;
+	}*/
+	
+
 }
