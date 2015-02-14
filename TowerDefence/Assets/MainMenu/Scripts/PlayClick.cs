37cc2fa84eebb974a39de7e6fc422fb06d5bfc18
William Meaton <will.meaton@gmail.com>
2015-02-14 11:08:48 +0000

    Implemented the Menu
    -Added menu buttons and onclick script

---
+++ TowerDefence/Assets/MainMenu/Scripts/PlayClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayClick : MonoBehaviour {
+	public string LevelToLoad;
+	// Use this for initialization
+	void OnMouseDown(){
+		Application.LoadLevel (LevelToLoad);
+	
+	}
+}
