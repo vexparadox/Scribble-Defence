89090a0146e57857502adab2d1158df54b9f89f0
William Meaton <will.meaton@gmail.com>
2015-02-27 15:11:22 +0000

    Fixed some issues with enemy collider

--- TowerDefence/Assets/Prefabs/Towers/Scripts/EnemyCollider.cs
+++ TowerDefence/Assets/Prefabs/Towers/Scripts/EnemyCollider.cs
@@ -5,7 +5,7 @@ public class EnemyCollider : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D collision) {
 			Destroy(collision.gameObject);
+			Destroy(gameObject);
 		}
-		Destroy (gameObject);
+		
 	}
-}
