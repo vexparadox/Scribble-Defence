bd63c798f46728bfadd8fb2fb9fb96ce492499fb
William Meaton <will.meaton@gmail.com>
2015-03-01 13:59:23 +0000

    Currency updates onscreen
    Got the UI text to update on screen when kills are made

--- TowerDefence/Assets/Map/Level1/Scripts/CurrencyScript.cs
+++ TowerDefence/Assets/Map/Level1/Scripts/CurrencyScript.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CurrencyScript : MonoBehaviour {
-	public int CurrentCurrency;
-	public int StartCurrency;
-	public int GruntWorth, TankWorth, SpeedyWorth, BossWorth;
-	public int Tower1Cost, Tower2Cost, Tower3Cost, Tower4Cost;
+	public int CurrentCurrency; //the current currecny, this is updated throughout the game - can later be private
+	public int StartCurrency; // this is the start currency of the level
+
+	public int GruntWorth, TankWorth, SpeedyWorth, BossWorth; // this is how much the user gets for each enemy kill
+	public int Tower1Cost, Tower2Cost, Tower3Cost, Tower4Cost; // this is how much each tower costs to place
+	public Text CurrencyLabel; // this is the Text object that holds the currecny label
+
 	void Start(){
-		CurrentCurrency = StartCurrency;
+		CurrentCurrency = StartCurrency; //assign the start currency
+		UpdateTextUI(); // update the TextUI to the current currency
 	}
 
 	void enemyDead(string EnemyName){
-		Debug.Log (EnemyName);
+		Debug.Log (EnemyName); // debug what enemy has been killed
+
+		//depending on what enemy is killed, add their worth to the currency
+		//enemy worth is defined publically in the _GM game object
 		if (EnemyName == "Grunt") {
 			CurrentCurrency += GruntWorth;
 		} else if (EnemyName == "Tank") {
@@ -21,6 +29,11 @@ public class CurrencyScript : MonoBehaviour {
 		} else if (EnemyName == "Boss") {
 			CurrentCurrency += BossWorth;
 		}
-		 
+		UpdateTextUI(); // update the TextUI to the current currency
+	}
+
+	void UpdateTextUI(){
+		// update the TextUI to the current currency
+		CurrencyLabel.text = "Cookies: " + CurrentCurrency;
 	}
 }
