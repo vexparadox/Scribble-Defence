8b079749ab52758aeffdf03726a717a564001f33
William Meaton <will.meaton@gmail.com>
2015-03-01 15:41:46 +0000

    Updated the way EnemyKilled is passed
    Swapped to arrays of integers instead of Strings, I think it's just a
    cleaner way of doing it

--- TowerDefence/Assets/Map/Level1/Scripts/CurrencyScript.cs
+++ TowerDefence/Assets/Map/Level1/Scripts/CurrencyScript.cs
@@ -6,7 +6,8 @@ public class CurrencyScript : MonoBehaviour {
 	public int CurrentCurrency; //the current currecny, this is updated throughout the game - can later be private
 	public int StartCurrency; // this is the start currency of the level
 
-	public int GruntWorth, TankWorth, SpeedyWorth, BossWorth; // this is how much the user gets for each enemy kill
+	public int[] EnemyWorth = new int[4]; // this is how much the user gets for each enemy kill
+	// element 0 = Grunt, 1 = Tank, 2 = Speedy, 3 = Boss
 	public int[] TowerCost = new int[4]; // this is how much each tower costs to place, element 0 = tower 1 etc.
  	public Text CurrencyLabel; // this is the Text object that holds the currecny label
 	public Text[] TowerCostLabel = new Text[4];
@@ -17,20 +18,11 @@ public class CurrencyScript : MonoBehaviour {
 		UpdateTextTowerCost ();
 	}
 
-	void enemyDead(string EnemyName){
-		Debug.Log (EnemyName); // debug what enemy has been killed
-
+	void enemyDead(int EnemyID){
 		//depending on what enemy is killed, add their worth to the currency
 		//enemy worth is defined publically in the _GM game object
-		if (EnemyName == "Grunt") {
-			CurrentCurrency += GruntWorth;
-		} else if (EnemyName == "Tank") {
-			CurrentCurrency += TankWorth;
-		} else if (EnemyName == "Speedy") {
-			CurrentCurrency += SpeedyWorth;
-		} else if (EnemyName == "Boss") {
-			CurrentCurrency += BossWorth;
-		}
+		CurrentCurrency += EnemyWorth[EnemyID]; 
+
 		UpdateTextCookiesUI(); // update the TextUI to the current currency
 	}
 
