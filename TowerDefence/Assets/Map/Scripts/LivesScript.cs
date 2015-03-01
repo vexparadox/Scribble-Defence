290fad662e85d3b1377296fd15338884c2c8c7bc
William Meaton <will.meaton@gmail.com>
2015-03-01 16:40:40 +0000

    Moved some scripts - added lives
    Added live system - collision isn't quite working yet, not sure why
    
    Moved scripts for levels

---
+++ TowerDefence/Assets/Map/Scripts/LivesScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LivesScript : MonoBehaviour {
+	public int StartLives; // holds the start amount of lives
+	public int CurrentLives; // holds the current amount of lives
+	public Text LivesLabel;
+		
+	// Use this for initialization
+	void Start () {
+		CurrentLives = StartLives;
+		UpdateLivesUI();
+	}
+	
+	// Update is called once per frame
+	public void TakeALife(){
+		CurrentLives--;
+		UpdateLivesUI();
+	}
+
+	void UpdateLivesUI(){
+		if (CurrentLives <= 0) {
+			Application.LoadLevel("MainMenu");
+				}
+		LivesLabel.text = "Lives: " + CurrentLives;
+	}
+}
