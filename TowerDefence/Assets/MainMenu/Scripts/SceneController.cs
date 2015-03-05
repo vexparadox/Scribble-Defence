dfc1f9b85b7f205cdc34cc38b3910954c51d164e
William Meaton <will.meaton@gmail.com>
2015-03-05 18:21:16 +0000

    Fixed a few things from that last push
    -Added no sleep to menu

---
+++ TowerDefence/Assets/MainMenu/Scripts/SceneController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.sleepTimeout = SleepTimeout.NeverSleep; // stop screen from sleeping
+	}
+}
