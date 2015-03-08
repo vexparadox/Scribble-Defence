da82e73b70fc30c4b1cef5480a154c1616cc0025
William Meaton <will.meaton@gmail.com>
2015-03-08 01:03:23 +0000

    Updated to latest 1.0.2
    Bug fixes!
    -UI Towers will now destroy bullets and bombs instead of having them bounce off (promise)
    Game Changes:
    -Changed AOE towers speed
    -Nerfed sniper tower
    -Nerfed invincible enemy tanks
    -Upped speedy enemy health
    -General bug fixes

--- Scribble Defence/Assets/Prefabs/Towers/Scripts/AmmoDestroyer.cs
+++ Scribble Defence/Assets/Prefabs/Towers/Scripts/AmmoDestroyer.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class AmmoDestroyer : MonoBehaviour {
 
-	void OnCollisionEnter2D(Collider2D other){
+	void OnCollisionEnter2D(Collision2D other){
 		if (other.gameObject.tag == "Bullet" || other.gameObject.tag == "Bomb") {
 			Destroy(other.gameObject);
 				}
