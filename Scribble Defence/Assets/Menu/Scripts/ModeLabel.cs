efe8e3b39ac85f1931b9bc3ede11f9f51b29033d
William Meaton <will.meaton@gmail.com>
2015-03-17 14:36:29 +0000

    Added more functionality to the menus

---
+++ Scribble Defence/Assets/Menu/Scripts/ModeLabel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ModeLabel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.GetInt ("HardMode") == 1) {
+			gameObject.GetComponent<Text>().text = "Hard Mode";
+				} else {
+			gameObject.GetComponent<Text>().text = "Normal Mode";
+			}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
