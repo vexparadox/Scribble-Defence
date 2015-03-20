738fe4b1a7de14e3c145611382627704bbf2b83d
William Meaton <will.meaton@gmail.com>
2015-03-20 16:54:33 +0000

    Made it harder

---
+++ Scribble Defence/Assets/Options/Scripts/SoundHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundHandler : MonoBehaviour {
+	private bool On;
+	public Sprite offSprite, onSprite;
+	
+	void Start(){
+		//make sure it matches the current setting
+		if (PlayerPrefs.GetInt("Sound") == 1) {
+			On = true;
+			gameObject.GetComponent<SpriteRenderer>().sprite = onSprite; //swap sprite
+			
+		} else {
+			On = false;
+			gameObject.GetComponent<SpriteRenderer>().sprite = offSprite; //swap the sprite
+		}
+	}
+	
+	void OnMouseUp(){
+		if (On) {
+			gameObject.GetComponent<SpriteRenderer>().sprite = offSprite; //swap the sprite
+			PlayerPrefs.SetInt("Sound", 0); //save for next time
+			On = false;
+		} else { 
+			gameObject.GetComponent<SpriteRenderer>().sprite = onSprite; //swap sprite
+			PlayerPrefs.SetInt("Sound", 1); //save for next time
+			On = true;
+		}
+	}
+}
