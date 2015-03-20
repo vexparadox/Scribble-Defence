14fee3835a9aa447239445f6c8d48425bd5e7f40
William Meaton <will.meaton@gmail.com>
2015-03-20 14:01:56 +0000

    Added speed up button

---
+++ Scribble Defence/Assets/FastForward.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FastForward : MonoBehaviour {
+
+	void OnMouseDown(){
+		Time.timeScale = 2.5f;
+	}
+
+	void OnMouseUp(){
+		Time.timeScale = 1;
+	}
+}
